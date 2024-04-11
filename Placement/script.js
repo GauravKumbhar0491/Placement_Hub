40be1492c275bae93ffc61684d1912e1c563a40e
BlackWizzieee <145113866+BlackWizzieee@users.noreply.github.com>
2024-04-11 18:29:56 +0530

    Add files via upload

---
+++ Placement/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Trigger the fade-in animation for the company list
+    var companyList = document.getElementById("companyList");
+    companyList.style.opacity = 1;
+});
