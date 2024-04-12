71a34322765490feddac6610a6f6c8b19ca640e0
BlackWizzieee <145113866+BlackWizzieee@users.noreply.github.com>
2024-04-12 23:53:58 +0530

    Delete Placement directory

--- Placement/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Trigger the fade-in animation for the company list
-    var companyList = document.getElementById("companyList");
-    companyList.style.opacity = 1;
-});
