ee845080d1e6b716888ed19df255898d836c31d3
Tenaya06 <166724690+Tenaya06@users.noreply.github.com>
2024-04-14 14:45:46 +0530

    updated files

--- Placement/app.py
+++ Placement/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,14 +8,34 @@ def main_page():
 
 @app.route('/stud_login')
 def stud_login():
-    return render_template('stud_login.html', error_html="<p style='color: red'>ERROR</p>")
+    return render_template('stud_login.html', signup=request.args.get('signup', False))
 
-@app.route('/profile')
+@app.route('/profile', methods=['GET', "POST"])
 def profile():
-    return render_template('profile.html')
+    if request.method == "POST":
+        data = dict(request.form)
+        
+        ssc= request.files['10ms']
+        hsc= request.files['12ms']
+        fe= request.files['fe']
+        se= request.files['se']
+        te= request.files['te']
+        be= request.files['be']
+        data.update({
+            "fe": fe.filename,
+            "se": se.filename,
+            "te": te.filename,
+            "be": be.filename,
+            "10ms": ssc.filename,
+            "12ms": hsc.filename
+        })
+        print(data)
+        return render_template('profile.html', data=data)
+    else:
+        return render_template('profile.html')
 
-@app.route('/dashboard')
-def dashboard():
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():      
     return render_template('dashboard.html')
 
 @app.route('/coordinator')
@@ -25,6 +45,12 @@ def coordinator():
 @app.route('/stat')
 def stat():
     return render_template('stat.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    print(request.form)
+    return redirect(url_for('stud_login', signup=True))
+
 
 
 
