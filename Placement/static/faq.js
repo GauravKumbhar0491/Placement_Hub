b06569c989901ce8519318887b7fc8a61f2c0337
BlackWizzieee <gaurav_kumbhar_aids@moderncoe.edu.in>
2024-04-16 01:04:48 +0530

    changes made

--- Placement/static/faq.js
+++ Placement/static/faq.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     accordionItems.forEach(item => {
         item.addEventListener("click", function() {
+            // Toggle active class on the clicked item
             this.classList.toggle("active");
+
+            // Close other accordion items
+            accordionItems.forEach(otherItem => {
+                if (otherItem !== item && otherItem.classList.contains("active")) {
+                    otherItem.classList.remove("active");
+                }
+            });
         });
     });
 });
