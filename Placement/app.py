f8be2c4fefc649dc8dc6b4274423c66a2b69779f
Gaurav Kumbhar <gauravkumbhar.work@gmail.com>
2026-03-02 14:31:05 +0530

    Update app.py

--- Placement/app.py
+++ Placement/app.py
@@ -205,5 +205,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    # Listen on all interfaces, port 5000
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
